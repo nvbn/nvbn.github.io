2366b6518bab9640bd2e7968eea8b8527af22db2
nvbn <nvbn.rm@gmail.com>
2016-01-10 19:58:32 +0300

    Use es6 in gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,17 @@
-var gulp = require('gulp');
-var shell = require('gulp-shell');
-var browserSync = require('browser-sync').create();
+"use strict";
+
+let gulp = require('gulp'),
+    shell = require('gulp-shell'),
+    browserSync = require('browser-sync').create();
 
 gulp.task('build', shell.task(['bundle exec jekyll build --watch']));
 
-gulp.task('serve', function () {
-    browserSync.init({server: {baseDir: '_site/'}});
-    gulp.watch('_site/**/*.*').on('change', browserSync.reload);
+gulp.task('serve', () => {
+    browserSync.init({
+        server: {baseDir: '_site/'},
+        open: false
+    });
+    gulp.watch('_site/**/*.html').on('change', browserSync.reload);
 });
 
 gulp.task('default', ['build', 'serve']);
