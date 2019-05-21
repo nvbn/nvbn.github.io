d44eceb6ef5e5200e34dfcce930de1f6d84b8a5a
Vladimir Iakovlev <nvbn.rm@gmail.com>
2019-05-21 21:31:26 +0200

    Add an article on Technology Strategy Patterns book

---
+++ _posts/2019-05-21-strategy-book.md
@@ -0,0 +1,17 @@
+---
+layout:     post
+title:      "Eben Hewitt: Technology Strategy Patterns"
+date:       2019-05-21 21:30:00
+keywords:   book
+image:      /assets/strategy_book.jpg
+---
+
+![book cover white](/assets/strategy_book.jpg) Not so long ago I wanted to read something
+about a large scale architecture and mistakenly decided that
+[Technology Strategy Patterns by Eben Hewitt](https://www.oreilly.com/library/view/technology-strategy-patterns/9781492040866/) was about that. It's not, but it was still interesting to read. The book provides
+a bunch of nice patterns for delivering information to product-people and stakeholders.
+The book covers a few ways of doing long-range planning and analysis with some examples from
+the real world. *It even has some Powerpoint and Excel advice.*
+
+Although somehow the book was useful for me and I've learned a bunch of new stuff, I'm definitely not
+the target audience of it.
--- gulpfile.js
+++ gulpfile.js
@@ -1,24 +1,28 @@
 "use strict";
 
-let gulp = require('gulp'),
-    shell = require('gulp-shell'),
-    browserSync = require('browser-sync').create(),
-    lastSync = new Date();
+const gulp = require("gulp"),
+  shell = require("gulp-shell"),
+  browserSync = require("browser-sync").create(),
+  lastSync = new Date();
 
-gulp.task('build', shell.task(['bundle exec jekyll build --watch']));
+const build = shell.task(["bundle exec jekyll build --watch --incremental"]);
 
-gulp.task('serve', () => {
-    browserSync.init({
-        server: {baseDir: '_site/'},
-        open: false
-    });
-    gulp.watch('_site/20*/**/*.html').on('change', (event) => {
-      const current = new Date();
-      if (current - lastSync > 500) {
-        lastSync = current;
-        browserSync.reload();
-      }
-    });
-});
+const serve = () => {
+  browserSync.init({
+    server: { baseDir: "_site/" },
+    open: false,
+  });
+  gulp.watch("_site/20*/**/*.html").on("change", event => {
+    const current = new Date();
+    if (current - lastSync > 500) {
+      lastSync = current;
+      browserSync.reload();
+    }
+  });
+};
 
-gulp.task('default', ['build', 'serve']);
+gulp.task("build", build);
+
+gulp.task("serve", serve);
+
+gulp.task("default", gulp.parallel("build", "serve"));
